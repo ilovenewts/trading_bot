84e6333c6a0ad8e9e408ed754314a73323c5ebfb
amphi <iloveamalamute@gmail.com>
2023-07-17 00:49:06 +0900

    print close price

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import os
+import time
+from datetime import datetime
+
+import ccxt
+from dotenv import load_dotenv
+
 
 def main():
-    print('Hello')
+    app: str = os.getenv('app')
+    if app == 'development':
+        load_dotenv()
+    access = os.getenv('ACCESS_KEY')
+    secret = os.getenv('SECRET_KEY')
+
+    binance = ccxt.binance(config={
+        'apiKey': access,
+        'secret': secret,
+        'enableRateLimit': True,
+        'options': {
+            'defaultType': 'future'
+        }
+    })
+
+    symbol = "BTC/USDT"
+
+    while True:
+        btc = binance.fetch_ticker(symbol)
+        now = datetime.now()
+        print(now, btc['last'])
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     main()
-
